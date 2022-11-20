81555ad5f88bb58dace85eb8fcf7e63267a47d17
Justin Irabor <justin.irabor@gmail.com>
2022-11-20 03:59:21 +0100

    string parser and test

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,14 +1,50 @@
 extern crate num;
 use num::Complex;
+use std::str::FromStr;
 
-#[allow(dead_code)]
-fn complex_square_add_loop(c: Complex<f64>) {
+/// Try to determine if `c` is in the Mandelbrot set, using at most `limit`
+/// iterations to decide.
+///
+/// If `c` is not a member, return `Some(i)`, where `i` is the number of
+/// iterations it took for `c` to leave the circle of radius two centered on the
+/// origin. If `c` seems to be a member (more precisely, if we reached the
+/// iteration limit without being able to prove that `c` is not a member),
+/// return `None`.
+
+fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32>{
     let mut z = Complex { re: 0.0, im: 0.0 };
-    loop {
+
+    for i in 0..limit {
         z = z * z + c;
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+    }
+
+    None
+}
+
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
+        None => None,
+        Some(index) => {
+            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+                (Ok(l), Ok(r)) => Some((l,r)),
+                _ => None
+            }
+        }
     }
 }
 
+#[test]
+fn test_parse_pair() {
+    assert_eq!(parse_pair::<i32>("", ','), None);
+    assert_eq!(parse_pair::<i32>("10", ','), None);
+    assert_eq!(parse_pair::<i32>(",10", ','), None);
+    assert_eq!(parse_pair::<i32>("10,20", ','), Some((10,20)));
+    assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
+    assert_eq!(parse_pair::<f64>("0.5x", 'x'), None);
+    assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
+}
 fn main() {
-    complex_square_add_loop(Complex { re: 0.0, im: 0.0 });
 }
